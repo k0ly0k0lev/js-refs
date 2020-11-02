cc49736b76d9d62e85b8a0509e5aff07a7326b59
k0ly0k0lev <kolyo.kolev.pft@gmail.com>
2020-11-02 09:41:24 +0200

    Update functions files naming style

--- src/functions/current_day.js
+++
@@ -1,8 +0,0 @@
-function currentDay() {
-    const DATE = new Date;
-    const DAY = DATE.getDay()
-    const DAY_LIST = ["Sunday", "Monday", "Tuesday", "Wednesday ", "Thursday", "Friday", "Saturday"]
-    return `The current day is ${DAY_LIST[DAY]}`
-}
-
-currentDay()
